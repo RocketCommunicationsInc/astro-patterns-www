4fb2e3587f55fafeb08f8e52e9111e738052c2fa
Mark Anthony Cianfrani <markacianf@gmail.com>
2023-09-14 10:59:08 -0400

    fix build

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Astro Nova",
   description: "Astro Nova",
+  ignoreDeadLinks: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-	purge: {
-	  enabled: process.env.NODE_ENV === 'production',
-	  content: [
+	content: [
 		'./docs/.vitepress/**/*.js',
 		'./docs/.vitepress/**/*.vue',
 		'./docs/.vitepress/**/**/*.vue',
 		'./docs/.vitepress/**/*.ts',
 		'./docs/**/*.md',
 	  ],
-	  options: {
-		safelist: ['html', 'body'],
-	  },
-	},
   }
